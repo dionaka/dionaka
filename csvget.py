f7f5e6de35ee5c80e75d887d4c29df51b06f136d
dionaka <158718748+dionaka@users.noreply.github.com>
2024-11-25 18:46:17 +0000

    yyy

--- csvget.py
+++ csvget.py
@@ -1,2 +1,17 @@
 import pandas as pd
-pd.read_excel("point_url.xlsx")
+import numpy as np
+
+
+def tran(url,title):
+    data = pd.read_excel(url)
+    data_copy = data[title].copy()
+    print(data_copy)
+
+
+def main():
+    url = ""
+    title = ""
+    tran(url,title)
+
+
+main
